dec9898bf5ba2b567912fe1b5ec0aaa3f7f39027
leonel47 <waboleonel@gmail.com>
2020-11-15 16:32:32 -0500

    Initial commit, project generated

---
+++ src/main/java/com/aiwa/hello/mongo/mongapp/MongappApplication.java
@@ -0,0 +1,13 @@
+package com.aiwa.hello.mongo.mongapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiwa/hello/mongo/mongapp/MongappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiwa.hello.mongo.mongapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
